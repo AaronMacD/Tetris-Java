10d28d3c5b20d553e7445ea6225cd329105c2f99
AJGameDev <ajmgamedev@gmail.com>
2023-03-12 16:31:11 -0400

    Minor fixes to gameboard and menu, added method stubs.

--- core/src/main/java/com/tetris/t6/GameBoard.java
+++ core/src/main/java/com/tetris/t6/GameBoard.java
@@ -1,6 +1,7 @@
 package com.tetris.t6;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -9,7 +10,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class GameBoard extends ApplicationAdapter {
+public class GameBoard extends Game {
     //Graphics
     private Texture blueBlock;
     private Texture cyanBlock;
@@ -58,6 +59,7 @@ public class GameBoard extends ApplicationAdapter {
 
     @Override
     public void render() {
+        super.render();
         Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
--- core/src/main/java/com/tetris/t6/Menus.java
+++ core/src/main/java/com/tetris/t6/Menus.java
@@ -1,4 +1,42 @@
 package com.tetris.t6;
 
-public class Menus {
+import com.badlogic.gdx.Screen;
+
+public class Menus implements Screen {
+
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
