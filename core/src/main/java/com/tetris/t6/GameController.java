3fbfb49d98f8677c2eb584bbafe045f7457105a1
dbjoe <josephzylla@gmail.com>
2023-03-14 14:25:12 -0400

    Remove pieceMaker method

--- core/src/main/java/com/tetris/t6/GameController.java
+++ core/src/main/java/com/tetris/t6/GameController.java
@@ -16,27 +16,4 @@ public class GameController {
 
     private Piece currentPiece;
 
-    private Piece PieceMaker() {
-        Random rand = new Random();
-        int num = rand.nextInt(6);
-        Piece p;
-
-        //create new piece based on random number
-        switch (num) {
-            case 0: p = new L_Piece();
-                break;
-            case 1: p = new Line_Piece();
-                break;
-            case 2: p = new S_Piece();
-                break;
-            case 3: p = new Square_Piece();
-                break;
-            case 4: p = new T_Piece();
-                break;
-            default: p = new Z_Piece();
-                break;
-        }
-
-        return p;
-    }
 }
