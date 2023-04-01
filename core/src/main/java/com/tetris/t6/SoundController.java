ce2eacfd9be2fec4c445a4fce7565976bd8c91d5
d <geeda@mail.gvsu.edu>
2023-04-01 14:13:46 -0400

    Added to SoundController to be able to take a "sound name" as a string to reference the asset name, and play it.

--- core/src/main/java/com/tetris/t6/SoundController.java
+++ core/src/main/java/com/tetris/t6/SoundController.java
@@ -1,4 +1,27 @@
 package com.tetris.t6;
 
-public class SoundController {
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
+
+public class SoundController extends ApplicationAdapter {
+    Sound sound;
+    String soundName;
+
+    public SoundController(String s) {
+        this.soundName = s;
+    }
+
+    public void create() {
+        this.sound = Gdx.audio.newSound(Gdx.files.internal(soundName));
+        this.sound.play(0.5f);
+
+    }
+
+    public void dispose() {
+    this.sound.dispose();
+    }
+
+
 }
