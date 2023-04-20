57775e3233ad2d24470e2ec020a2ec5a39e1860e
dbjoe <josephzylla@gmail.com>
2023-04-20 16:59:04 -0400

    Create package-info

---
+++ core/src/main/java/com/tetris/t6/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This module contains the code for a modern Tetris game made using libGDX.
+ * <p>
+ * This module contains the logic and GUI classes for the game that are
+ * instantiated in a separate libGDX Lwjgl3Launcher module.
+ * </p>
+ *
+ * @since 1.0
+ * @author Jacob Fidler, Darryl Gee, Aaron MacDougall, Joseph Zylla
+ * @version 1.1
+ */
+package com.tetris.t6;
